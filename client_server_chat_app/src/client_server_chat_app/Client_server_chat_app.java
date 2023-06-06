f41c64ce0038bd3e9a78c65aa0c3f723bacf7eab
rashidasohail <69976325+rashidasohail@users.noreply.github.com>
2023-06-06 08:23:47 +0500

    Add files via upload

---
+++ client_server_chat_app/src/client_server_chat_app/Client_server_chat_app.java
@@ -0,0 +1,17 @@
+
+package client_server_chat_app;
+
+/**
+ *
+ * @author Rashda Khanzada
+ */
+public class Client_server_chat_app {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
