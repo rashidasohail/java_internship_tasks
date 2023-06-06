7482e9cff82140364160ebc257593c84f0bfd661
rashidasohail <69976325+rashidasohail@users.noreply.github.com>
2023-06-06 08:46:08 +0500

    Add files via upload

---
+++ My_Travel_Cost/src/my_travel_cost/My_Travel_Cost.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package my_travel_cost;
+
+/**
+ *
+ * @author Rashda Khanzada
+ */
+public class My_Travel_Cost {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
