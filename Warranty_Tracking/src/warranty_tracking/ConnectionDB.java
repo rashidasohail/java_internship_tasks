52bf5b3744a9fb4a3ab9b6fa57a69e894a198a55
rashidasohail <69976325+rashidasohail@users.noreply.github.com>
2023-06-06 08:57:08 +0500

    Add files via upload

---
+++ Warranty_Tracking/src/warranty_tracking/ConnectionDB.java
@@ -0,0 +1,29 @@
+package warranty_tracking;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Rashda Khanzada
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+public class ConnectionDB {
+    public static Connection ConnectDb(){
+    try{
+         Class.forName("oracle.jdbc.driver.OracleDriver");
+         Connection conn=(Connection) DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl","scott","scott");
+         System.out.println("Connected");
+         return conn;
+    }
+    catch(Exception e){
+         JOptionPane.showMessageDialog(null,e);
+         return null;
+    }
+ }
+}
