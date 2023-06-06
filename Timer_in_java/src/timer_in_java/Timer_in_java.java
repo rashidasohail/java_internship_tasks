4788dc20b2df3bcb4183eecd9f563b107f3ef30a
rashidasohail <69976325+rashidasohail@users.noreply.github.com>
2023-06-06 08:53:54 +0500

    Add files via upload

---
+++ Timer_in_java/src/timer_in_java/Timer_in_java.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package timer_in_java;
+
+/**
+ *
+ * @author Rashda Khanzada
+ */
+public class Timer_in_java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ TrafficCrimeManagement/src/trafficcrimemanagement/TrafficCrimeManagement.java
@@ -0,0 +1,16 @@
+package trafficcrimemanagement;
+
+/**
+ *
+ * @author Rashda Khanzada
+ */
+public class TrafficCrimeManagement {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
