ba6bc1d61e437f909d8d452be505025d22b8a75f
rashidasohail <69976325+rashidasohail@users.noreply.github.com>
2023-06-06 08:42:50 +0500

    Add files via upload

---
+++ loginpage/src/loginpage/ConnectionDB.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package loginpage;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Rashda Khanzada
+ */
+public class ConnectionDB {
+    public static Connection ConnectDb(){
+    try{
+    Class.forName("oracle.jdbc.driver.OracleDriver");
+    Connection conn=(Connection) DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl","scott","scott");
+    System.out.println("Connected");
+    return conn;
+    }
+    catch(Exception e){
+    JOptionPane.showMessageDialog(null,e);
+    return null;
+    }
+    }
+    
+}
+
